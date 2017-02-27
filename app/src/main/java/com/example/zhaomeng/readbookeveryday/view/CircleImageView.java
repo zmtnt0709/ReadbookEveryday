0d14453b9b9150f2e6fbe1250487f3341f1fca11
zmtnt0709 <312601463@qq.com>
2017-02-27 17:26:27 +0800

    1、圆形图片增加白色圆环

--- app/src/main/java/com/example/zhaomeng/readbookeveryday/view/CircleImageView.java
+++ app/src/main/java/com/example/zhaomeng/readbookeveryday/view/CircleImageView.java
@@ -12,6 +12,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.example.zhaomeng.readbookeveryday.util.DisplayUtil;
+
 /**
  * 创建时间：2017/02/06 17:03 <br>
  * 作者：zhaomeng <br>
@@ -21,8 +23,17 @@ import android.widget.ImageView;
 public class CircleImageView extends ImageView {
     private static String TAG = CircleImageView.class.getSimpleName();
 
-    private Paint paint;
+    private static int RING_WIDTH = 3;//外圈圆环宽度，单位dp；
+
+    private int centerX;
+    private int centerY;
+    private int ringRadius;
+    private int imageRadius;
+    private Paint paintBitmap;
+    private Paint paintImage;
+    private Paint paintRing;
     private Canvas circleCanvas;
+    private Bitmap circleBitmap;
 
     public CircleImageView(Context context) {
         super(context);
@@ -40,15 +51,32 @@ public class CircleImageView extends ImageView {
     }
 
     private void initData() {
-        paint = new Paint();
+        paintBitmap = new Paint();
+        paintImage = new Paint();
+        paintImage.setAntiAlias(true);
+        paintRing = new Paint();
+        paintRing.setAntiAlias(true);
+        paintRing.setColor(0xffffffff);
+        paintRing.setStrokeWidth(DisplayUtil.dip2px(getContext(), RING_WIDTH));
+        paintRing.setStyle(Paint.Style.STROKE);
         circleCanvas = new Canvas();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        Log.d(TAG, "onSizeChanged");
+        centerX = w / 2;
+        centerY = h / 2;
+        int radius = w > h ? h / 2 : w / 2;
+        ringRadius = radius - DisplayUtil.dip2px(getContext(), RING_WIDTH) / 2;
+        imageRadius = radius - DisplayUtil.dip2px(getContext(), RING_WIDTH) + 1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Log.d(TAG, "onDraw");
         Drawable drawable = getDrawable();
-        if (null != drawable) {
+        if (drawable != null) {
             drawCirclePicture(drawable, canvas);
         } else {
             super.onDraw(canvas);
@@ -59,28 +87,31 @@ public class CircleImageView extends ImageView {
      * 画圆形图片
      */
     private void drawCirclePicture(Drawable drawable, Canvas canvas) {
-        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap b = getCircleBitmap(bitmap);
-        paint.reset();
-        canvas.drawBitmap(b, 0, 0, paint);
+        //画圆形图片
+        circleBitmap = getCircleBitmap(drawable);
+        canvas.drawBitmap(circleBitmap, 0, 0, paintImage);
+        //画白色圆环
+        canvas.drawCircle(centerX, centerY, ringRadius, paintRing);
     }
 
     /**
      * 获取圆形图片方法
      *
-     * @param bitmap 传入方形图片
+     * @param drawable 传入方形图片
      * @return Bitmap  输出圆形图片
      */
-    private Bitmap getCircleBitmap(Bitmap bitmap) {
+    private Bitmap getCircleBitmap(Drawable drawable) {
+        Log.d(TAG, "width = " + getWidth() + ", height = " + getHeight());
+        Bitmap input = ((BitmapDrawable) drawable).getBitmap();
         Bitmap output = Bitmap.createBitmap(getWidth(),
                 getHeight(), Bitmap.Config.ARGB_8888);
+        paintBitmap.reset();
+        paintBitmap.setAntiAlias(true);
         circleCanvas.setBitmap(output);
-        paint.setAntiAlias(true);
         circleCanvas.drawARGB(0, 0, 0, 0);
-        int radius = getWidth() / 2;
-        circleCanvas.drawCircle(radius, radius, radius, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        circleCanvas.drawBitmap(bitmap, getImageMatrix(), paint);
+        circleCanvas.drawCircle(centerX, centerY, imageRadius, paintBitmap);
+        paintBitmap.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        circleCanvas.drawBitmap(input, getImageMatrix(), paintBitmap);
         return output;
     }
 }
